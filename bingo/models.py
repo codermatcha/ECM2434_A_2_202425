f3f45971da38c6d97fbbe8eddc8bf863c4b4df88
Laurenconz <laurennchao@gmail.com>
2025-02-23 14:25:07 +0000

    react router

--- bingo/models.py
+++ bingo/models.py
@@ -1,6 +1,35 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
+
+class Reward(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    points_required = models.IntegerField()  # Number of points needed to claim
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class UserChallenge(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    challenge_name = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.challenge_name}"
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class User(AbstractUser):
     PROFILE_CHOICES = [
