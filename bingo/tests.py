91c2284d0ad4ae498d0980ba47a84955e455f503
Siemon-Cha <sc1332@exeter.ac.uk>
2025-02-23 17:55:20 +0000

    import TestCase and views

--- bingo/tests.py
+++ bingo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .views import email_validation
+
+class EmailValidationTest(TestCase):
+    def assertEmailValidation(self, email, expected_result, msg):
+        """Helper method to test email validation."""
+        result = email_validation(email)
+        self.assertEqual(result, expected_result, msg)
 
 # Create your tests here.
